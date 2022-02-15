4018e86b84f1faf00797a538a9b03e8bfff0b375
noobdev95 <albinsraj2021@gmail.com>
2022-02-15 15:05:24 +0530

    RMX2170: HavocOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2020 The DotOS
+# Copyright (C) 2022 The HavocOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX2170.mk
+    $(LOCAL_DIR)/havoc_RMX2170.mk
+   
+COMMON_LUNCH_CHOICES := \
+     havoc_RMX2170-userdebug \
+     havoc_RMX2170-user \
+     havoc_RMX2170-eng
