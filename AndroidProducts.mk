ab501de0a3fee36fb9715fb2947bbdf0dd116f06
noobdev95 <albinsraj2021@gmail.com>
2022-03-06 17:58:33 +0000

    RMX2170: AncientOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The Nusantara Project
+# Copyright (C) 2022 The AncientOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_RMX2170.mk
+    $(LOCAL_DIR)/ancient_RMX2170.mk
    
 COMMON_LUNCH_CHOICES := \
-     nad_RMX2170-userdebug \
-     nad_RMX2170-user \
-     nad_RMX2170-eng
+     ancient_RMX2170-userdebug \
+     ancient_RMX2170-user \
+     ancient_RMX2170-eng
